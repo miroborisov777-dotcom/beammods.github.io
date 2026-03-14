23322c954a8a58f0b16704a0c7342bedc2368a95
miroborisov777-dotcom <miroborisov777@gmail.com>
2026-03-14 20:56:46 +0200

    Remove download function and add searchMods function

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function download(){
-alert("Download starting...");
+function searchMods() {
+
+let input = document.getElementById("search").value.toLowerCase();
+let mods = document.getElementsByClassName("mod-card");
+
+for (let i = 0; i < mods.length; i++) {
+
+let title = mods[i].innerText.toLowerCase();
+
+if (title.includes(input)) {
+mods[i].style.display = "";
+} else {
+mods[i].style.display = "none";
+}
+
+}
+
 }
